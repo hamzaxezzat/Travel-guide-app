96dfa6b40e9ec6edc9bb062c6ac9fa0178277e19
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-22 22:37:54 +0200

    init all base files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
-import "./style.css";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 export default function App() {
-    return <h1>Hello World</h1>;
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Grid container spacing={3} style={{ width: "100%" }}>
+                <Grid item xs={12} md={4}>
+                    <List />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map />
+                </Grid>
+            </Grid>
+        </>
+    );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function Header() {
-    return <h1>dsadsa</h1>;
-}
+const Header = () => {
+    return <h1>Header</h1>;
+};
+
+export default Header;
