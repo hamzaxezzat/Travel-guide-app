d4bae4f7b41de32b45dca3e84b0cc71ff301ae31
hamzaezzzat <hamza2007ezzat@gmail.com>
2022-05-22 23:11:09 +0200

    finish Maps

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import GoogleMapReact from "google-map-react";
+import { Paper, Typography, useMediaQuery } from "@material-ui/core";
+import { LocationOnOutlinedIcon } from "@material-ui/icons/LocationOnOutlined";
+import Rating from "@material-ui/lab";
 
+import useStyles from "./style";
 const Map = () => {
-    return <h1>Map</h1>;
+    const classes = useStyles();
+    const isMobile = useMediaQuery("(min-width:600px)"); // isMobile = false if min-width:600px
+    const coordinates = { lat: 0, lng: 0 }; //attitude & Langtude
+    return (
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: "AIzaSyD2dc55GcNck7S8akJzeOKOSTmGkb6HMhY",
+                }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50, 50, 50, 50]}
+                options={""}
+                onChange={""}
+                onChildClick={""}
+            ></GoogleMapReact>
+        </div>
+    );
 };
 
 export default Map;
